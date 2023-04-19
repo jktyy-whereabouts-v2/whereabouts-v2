ac989067f7f08e43868b2a1f5f8547d74ce8c0b4
Kevin <kevinyoon01@gmail.com>
2023-04-18 19:42:18 -0700

    App.tsx and pages in progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,37 @@
+import React, { Component, useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Header from './components/Header';
+import StartPage from './components/pages/StartPage';
+import Login from './components/pages/Login';
+import Registration from './components/pages/Registration';
+import Dashboard from './components/pages/Dashboard';
+import ChatPage from './components/ChatPage';
+
 function App() {
+	interface User {
+		name: string;
+		phone_number: string;
+		password: string;
+	}
+
+	const [userInfo, setUserInfo] = useState<User>({
+		name: '',
+		phone_number: '',
+		password: '',
+	});
+
 	return (
 		<>
-			<div>test</div>
+			<Router>
+				<Header />
+				<Routes>
+					<Route path="/" element={<StartPage userInfo={userInfo} setUserInfo={setUserInfo} />} />
+					<Route path="/login" element={<Login userInfo={userInfo} setUserInfo={setUserInfo} />} />
+					<Route path="/register" element={<Registration userInfo={userInfo} setUserInfo={setUserInfo} />} />
+					<Route path="/dashboard" element={<Dashboard userInfo={userInfo} setUserInfo={setUserInfo} />} />
+					<Route path="/chat" element={<ChatPage userInfo={userInfo} />} />
+				</Routes>
+			</Router>
 		</>
 	);
 }
---
+++ client/src/components/pages/StartPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function StartPage() {
+	return <div>StartPage</div>;
+}
+
+export default StartPage;
