c6187e6e1009cffd2767459ba28ad8e0b9822456
Kevin <kevinyoon01@gmail.com>
2023-04-20 21:25:26 -0700

    Register/Login in progress

--- TS_src/client/src/App.tsx
+++ TS_src/client/src/App.tsx
@@ -1,63 +1,44 @@
-import React, { Component, useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Header from "./components/Header";
-import StartPage from "./components/pages/StartPage";
-import Login from "./components/pages/Login";
-import Registration from "./components/pages/Registration";
-import Dashboard from "./components/pages/Dashboard";
-import ChatPage from "./components/ChatPage";
-import { User } from "./components/types";
-import socket from "./main";
+import React, { Component, useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Header from './components/Header';
+import StartPage from './components/pages/StartPage';
+import Login from './components/pages/Login';
+import Registration from './components/pages/Registration';
+import Dashboard from './components/pages/Dashboard';
+import ChatPage from './components/ChatPage';
+import { User } from './components/types';
+import socket from './main';
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
-  const [userInfo, setUserInfo] = useState<User>({
-    name: "",
-    phone_number: "",
-    password: "",
-  });
+	const [userInfo, setUserInfo] = useState<User>({
+		name: '',
+		phone_number: '',
+		password: '',
+	});
 
-  return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <StartPage userInfo={userInfo} setUserInfo={setUserInfo} />
-            }
-          />
-          <Route
-            path="/login"
-            element={<Login userInfo={userInfo} setUserInfo={setUserInfo} />}
-          />
-          <Route
-            path="/register"
-            element={
-              <Registration userInfo={userInfo} setUserInfo={setUserInfo} />
-            }
-          />
-          <Route
-            path="/dashboard"
-            element={
-              <Dashboard userInfo={userInfo} setUserInfo={setUserInfo} />
-            }
-          />
-          <Route
-            path="/chat"
-            element={
-              <ChatPage
-                userInfo={userInfo}
-                setUserInfo={setUserInfo}
-                path="/chat"
-                socket={socket}
-              />
-            }
-          />
-        </Routes>
-      </Router>
-    </>
-  );
+	const login = (userInfo: any) => {
+		// console.log(userData);
+		setUserInfo({ name: userInfo.name, phone_number: userInfo.phone_number, password: userInfo.password });
+		localStorage.setItem('user', JSON.stringify(userInfo));
+		console.log('logged in confirmed');
+	};
+
+	return (
+		<>
+			<Router>
+				{/* <Header /> */}
+				<Routes>
+					<Route path="/" element={<StartPage userInfo={userInfo} setUserInfo={setUserInfo} />} />
+					<Route path="/login" element={<Login userInfo={userInfo} setUserInfo={setUserInfo} login={login} />} />
+					<Route path="/register" element={<Registration userInfo={userInfo} setUserInfo={setUserInfo} login={login} />} />
+					<Route path="/dashboard" element={<Dashboard userInfo={userInfo} setUserInfo={setUserInfo} />} />
+					<Route path="/chat" element={<ChatPage userInfo={userInfo} setUserInfo={setUserInfo} path="/chat" socket={socket} />} />
+				</Routes>
+			</Router>
+			<Toaster />
+		</>
+	);
 }
 
 export default App;
--- TS_src/client/src/components/pages/Login.tsx
+++ TS_src/client/src/components/pages/Login.tsx
@@ -1,99 +1,111 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import {
-  Link,
-  useNavigate,
-} from "react-router-dom";
-import { Button, TextField } from "@mui/material";
-import { User, UserProps } from "../types";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import { Button, TextField, Paper } from '@mui/material';
+import { User, UserProps } from '../types';
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { toast } from 'react-hot-toast';
+import 'react-toastify/dist/ReactToastify.css';
 
-const LOGIN_URL = "/api/login";
+const LOGIN_URL = '/api/login';
 
-function Login({ userInfo, setUserInfo }: UserProps) {
-  const navigate = useNavigate();
+interface LoginProps {
+	userInfo: User;
+	setUserInfo: React.Dispatch<React.SetStateAction<User>>;
+	login: Function;
+}
 
-  // hook to redirect after successful login
-  const [redirect, setRedirect] = useState<boolean>(false);
+function Login({ userInfo, setUserInfo, login }: LoginProps) {
+	const navigate = useNavigate();
 
-  const handleSubmit = async (event: React.SyntheticEvent) => {
-    event.preventDefault();
-    try {
-      const response = await axios.post(
-        LOGIN_URL,
-        JSON.stringify({ userInfo }),
-        {
-          headers: { "Content-Type": "application/json" },
-          withCredentials: true,
-        }
-      );
-      if (response.data) {
-        setRedirect(true);
-      }
-    } catch (error) {
-      if (error) {
-        alert(`Please check login information and try again`);
-      }
-    }
-  };
+	// hook to redirect after successful login
+	const [redirect, setRedirect] = useState<boolean>(false);
 
-  useEffect(() => {
-    if (redirect) {
-      navigate("/dashboard");
-    }
-  });
+	const handleSubmit = async (event: React.SyntheticEvent) => {
+		event.preventDefault();
 
-  return (
-    <div className="login-container">
-      <form onSubmit={handleSubmit} className="form-container">
-        <br></br>
-        <h3>Already a member?</h3>
-        <h3>Please Login</h3>
-        <br></br>
-        <div className="input-container">
-          <br></br>
-          <TextField
-            type="text"
-            className="input-box"
-            id="phone_number"
-            name="phone_number"
-            label="login phone number"
-            size="small"
-            required={true}
-            value={userInfo.phone_number}
-            onChange={(e) =>
-              setUserInfo({ ...userInfo, phone_number: e.target.value })
-            }
-          />
-        </div>
-        <div className="input-container">
-          <br></br>
-          <TextField
-            type="password"
-            id="password"
-            className="input-box"
-            name="password"
-            label="password"
-            size="small"
-            required={true}
-            value={userInfo.password}
-            onChange={(e) =>
-              setUserInfo({ ...userInfo, password: e.target.value })
-            }
-          />
-        </div>
-        <br></br>
-        <Button type="submit" className="submit-btn" variant="contained">
-          Submit Login
-        </Button>
-        <br></br>
-      </form>
+		const userLogin = {
+			phone_number: userInfo.phone_number,
+			password: userInfo.password,
+		};
+		try {
+			const response = await axios.post(LOGIN_URL, userLogin);
+			if (response.status === 200) {
+				console.log(response.data);
+				setRedirect(true);
+				login(response.data);
+			}
+		} catch (error) {
+			toast.error(`Please check login information and try again`);
+		}
+	};
 
-      <p>
-        Don't have a login yet?
-        <Link to="/register"> Sign up here!</Link>
-      </p>
-    </div>
-  );
+	useEffect(() => {
+		if (redirect) {
+			navigate('/dashboard');
+		}
+	});
+
+	const paperStyle = { padding: 40, height: '70vh', width: 375, margin: '100px auto' };
+
+	return (
+		<>
+			<Container maxWidth="xs">
+				<CssBaseline />
+				<Paper elevation={2} style={paperStyle}>
+					<Box
+						sx={{
+							marginTop: 8,
+							display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'center',
+						}}>
+						<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+							<LockOutlinedIcon />
+						</Avatar>
+						<Typography variant="h5">Login</Typography>
+						<Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+							<TextField
+								margin="normal"
+								required
+								fullWidth
+								id="email"
+								label="Phone Number"
+								name="phone_number"
+								onChange={(e) => setUserInfo({ ...userInfo, phone_number: e.target.value })}
+								value={userInfo.phone_number}
+							/>
+							<TextField
+								margin="normal"
+								required
+								fullWidth
+								name="password"
+								label="Password"
+								type="password"
+								id="password"
+								onChange={(e) => setUserInfo({ ...userInfo, password: e.target.value })}
+								value={userInfo.password}
+							/>
+							<Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+								Sign In
+							</Button>
+							<Grid container>
+								<Grid item>
+									<Link to="/register">{"Don't have an account? Sign Up"}</Link>
+								</Grid>
+							</Grid>
+						</Box>
+					</Box>
+				</Paper>
+			</Container>
+		</>
+	);
 }
 
 export default Login;
