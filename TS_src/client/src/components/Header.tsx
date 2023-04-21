83468a5e56f6568f39e623a2c8c7a3f884071b78
Kevin <kevinyoon01@gmail.com>
2023-04-20 17:50:51 -0700

    .env updated

--- TS_src/client/src/components/Header.tsx
+++ TS_src/client/src/components/Header.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
-  return (
-    <header>
-      <Link to="/">
-        <img src={require("/client/assets/logo.png")} />
-      </Link>
-    </header>
-  );
+	// return (
+	//   <header>
+	//     <Link to="/">
+	//       <img src={require("/client/assets/logo.png")} />
+	//     </Link>
+	//   </header>
+	// );
 }
 
 export default Header;
