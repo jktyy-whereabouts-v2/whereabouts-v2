54a06e86d09f2ca97373f343671f7223a9ae77be
Ted Chu <tchu002@gmail.com>
2023-04-23 17:51:27 -0700

    updated MapContainer to render TripImWatching

--- TS_src/client/src/components/Contacts.tsx
+++ TS_src/client/src/components/Contacts.tsx
@@ -1,160 +1,211 @@
-import React, { useState, useEffect, Dispatch, SetStateAction } from 'react';
-import { CssBaseline, Container, Box, Typography, TextField, IconButton, Button } from '@mui/material';
-import '@fontsource/roboto/300.css';
-import SearchIcon from '@mui/icons-material/Search';
-import axios from 'axios';
-import ContactList from './ContactList';
+import React, { useState, useEffect, Dispatch, SetStateAction } from "react";
+import {
+  CssBaseline,
+  Container,
+  Box,
+  Typography,
+  TextField,
+  IconButton,
+  Button,
+} from "@mui/material";
+import "@fontsource/roboto/300.css";
+import SearchIcon from "@mui/icons-material/Search";
+import axios from "axios";
+import ContactList from "./ContactList";
 // import { Routes, Route } from "react-router-dom";
 // import MyTripCard from "./MyTripCard";
-import { User, Trip } from './types';
-import Sidebar from './Sidebar';
-import Divider from '@mui/material/Divider';
-import { useNavigate } from 'react-router-dom';
+import { User, Trip } from "./types";
+import Sidebar from "./Sidebar";
+import Divider from "@mui/material/Divider";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
-	userInfo: User;
-	contacts: Array<User>;
-	setContacts: Dispatch<SetStateAction<Array<User>>>;
-	setActiveComponent: Dispatch<SetStateAction<string>>;
-	logout: Function;
-	setUserTrip: Dispatch<SetStateAction<Trip>>;
+  userInfo: User;
+  contacts: Array<User>;
+  setContacts: Dispatch<SetStateAction<Array<User>>>;
+  setActiveComponent: Dispatch<SetStateAction<string>>;
+  logout: Function;
+  setUserTrip: Dispatch<SetStateAction<Trip>>;
 }
 
 const googleURL = process.env.GOOGLEMAPSAPIKEY;
 
-function Contacts({ userInfo, contacts, setContacts, setActiveComponent, logout, setUserTrip }: Props) {
-	const navigate = useNavigate();
+function Contacts({
+  userInfo,
+  contacts,
+  setContacts,
+  setActiveComponent,
+  logout,
+  setUserTrip,
+}: Props) {
+  const navigate = useNavigate();
 
-	// hook to manage contacts checked from list
+  // hook to manage contacts checked from list
 
-	const [checkedContacts, setCheckedContacts] = useState<User[]>([]);
-	const [buttonDisabled, setButtonDisabled] = useState<boolean>(true);
-	const [contactData, addContactData] = useState([]);
-	const [submitted, clickSubmitted] = useState(false);
+  const [checkedContacts, setCheckedContacts] = useState<User[]>([]);
+  const [buttonDisabled, setButtonDisabled] = useState<boolean>(true);
+  const [contactData, addContactData] = useState([]);
+  const [submitted, clickSubmitted] = useState(false);
 
-	// Fetch GET request for contact and add to list:
-	const handleSubmit = async (event: any) => {
-		event.preventDefault();
-		//console.log('submit: ', event.target[0].value )
-		const phone_number = event.target[0].value.replaceAll(/[^0-9]/g, '');
-		event.target[0].value = '';
-		//fetch request to get contact info
-		try {
-			const response = await axios.get(`/api/users/${phone_number}`);
+  // Fetch GET request for contact and add to list:
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    // console.log("submit: ", event.target[0].value);
+    const phone_number = event.target[0].value.replaceAll(/[^0-9]/g, "");
+    event.target[0].value = "";
+    //fetch request to get contact info
+    try {
+      const response = await axios.get(`/api/users/${phone_number}`);
+      // console.log(response.data);
+      const contactData = response.data[0];
+      if (!contactData.name) return;
 
-			const contactData = response.data[0];
-			if (!contactData.name) return;
+      // add user to array of contacts
+      const contactShown = contacts.reduce((acc, user) => {
+        if (user.phone_number === contactData.phone_number) ++acc;
+        return acc;
+      }, 0);
+      if (!contactShown) setContacts([...contacts, contactData]);
+    } catch (err: any) {
+      console.log(
+        "Fetch request for user with phone_number failed.",
+        err.response.data
+      );
+    }
+  };
 
-			// add user to array of contacts
-			const contactShown = contacts.reduce((acc, user) => {
-				if (user.phone_number === contactData.phone_number) ++acc;
-				return acc;
-			}, 0);
-			if (!contactShown) setContacts([...contacts, contactData]);
-		} catch (err: any) {
-			console.log('Fetch request for user with phone_number failed.', err.response.data);
-		}
-	};
+  // function to delete contact from list, pass to contacts list
+  const deleteContact = (index: number, contact: User) => {
+    const newContacts = [...contacts];
+    const newCheckedContacts = [...checkedContacts];
+    newContacts.splice(index, 1);
 
-	// function to delete contact from list, pass to contacts list
-	const deleteContact = (index: number, contact: User) => {
-		const newContacts = [...contacts];
-		const newCheckedContacts = [...checkedContacts];
-		newContacts.splice(index, 1);
+    const foundIndex = newCheckedContacts.indexOf(contact);
+    if (foundIndex >= 0) newCheckedContacts.splice(foundIndex, 1);
+    if (newContacts.length === 0 || newCheckedContacts.length === 0)
+      setButtonDisabled(true);
 
-		const foundIndex = newCheckedContacts.indexOf(contact);
-		if (foundIndex >= 0) newCheckedContacts.splice(foundIndex, 1);
-		if (newContacts.length === 0 || newCheckedContacts.length === 0) setButtonDisabled(true);
+    // console.log('new contacts:', newContacts);
+    // console.log('new checked contacts', newCheckedContacts)
+    setContacts(newContacts);
+    setCheckedContacts(newCheckedContacts);
+  };
 
-		// console.log('new contacts:', newContacts);
-		// console.log('new checked contacts', newCheckedContacts)
-		setContacts(newContacts);
-		setCheckedContacts(newCheckedContacts);
-	};
+  // function to extract phone numbers from checkedContacts array
+  const extractPhoneNumbers = (array: typeof contacts) => {
+    return array.map((obj) => obj.phone_number);
+  };
 
-	// function to extract phone numbers from checkedContacts array
-	const extractPhoneNumbers = (array: typeof contacts) => {
-		return array.map((obj) => obj.phone_number);
-	};
+  // declare variable to contain proper info to send backend
+  const tripData = {
+    traveler: userInfo.phone_number,
+    watchers: extractPhoneNumbers(checkedContacts),
+  };
 
-	// declare variable to contain proper info to send backend
-	const tripData = {
-		traveler: userInfo.phone_number,
-		watchers: extractPhoneNumbers(checkedContacts),
-	};
+  // function to send post request to back end with user information to start trip
+  const handleStartTrip = async () => {
+    // create a post request to the route: /api/trips/start
+    try {
+      const response = await axios.post(
+        `https://www.googleapis.com/geolocation/v1/geolocate?key=${googleURL}`
+      );
+      const { lat, lng } = response.data.location;
+      setUserTrip((prevState: any) => {
+        return {
+          ...prevState,
+          start_timestamp: new Date().toDateString(),
+          start_lat: lat,
+          start_lng: lng,
+        };
+      });
+      const res = await axios.post("/api/trips/start", tripData);
+      if (res.status === 204) {
+        console.log("status is 200, redirect to MyTripCard");
+        clickSubmitted(true);
+      }
+    } catch (error) {
+      if (error) {
+        alert("Please check contacts information and try again");
+      }
+    }
+  };
 
-	// function to send post request to back end with user information to start trip
-	const handleStartTrip = async () => {
-		// create a post request to the route: /api/trips/start
-		try {
-			const response = await axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${googleURL}`);
-			const { lat, lng } = response.data.location;
-			setUserTrip((prevState: any) => {
-				return {
-					...prevState,
-					start_timestamp: new Date().toDateString(),
-					start_lat: lat,
-					start_lng: lng,
-				};
-			});
-			const res = await axios.post('/api/trips/start', tripData);
-			if (res.status === 204) {
-				console.log('status is 200, redirect to MyTripCard');
-				clickSubmitted(true);
-			}
-		} catch (error) {
-			if (error) {
-				alert('Please check contacts information and try again');
-			}
-		}
-	};
+  useEffect(() => {
+    if (submitted) {
+      navigate("/myTrip");
+    }
+  }, [submitted]);
 
-	useEffect(() => {
-		if (submitted) {
-			navigate('/myTrip');
-		}
-	}, [submitted]);
+  // // checking state of contacts data:
+  useEffect(() => {
+    // console.log('Current checkedContacts:', checkedContacts);
+    // console.log('Current User phone: ', userInfo.phone_number);
+    // console.log('Current trip data: ', tripData);
+  }, [checkedContacts, userInfo.phone_number, tripData]);
 
-	// // checking state of contacts data:
-	useEffect(() => {
-		// console.log('Current checkedContacts:', checkedContacts);
-		// console.log('Current User phone: ', userInfo.phone_number);
-		// console.log('Current trip data: ', tripData);
-	}, [checkedContacts, userInfo.phone_number, tripData]);
+  return (
+    <>
+      <Divider sx={{ width: "85%", margin: "auto" }} variant="middle"></Divider>
+      <CssBaseline />
+      <Box sx={{ display: "flex", mt: "30px" }}>
+        <Container sx={{ width: "40%", ml: "30px" }}>
+          <Sidebar logout={logout} />
+        </Container>
 
-	return (
-		<>
-			<Divider sx={{ width: '85%', margin: 'auto' }} variant="middle"></Divider>
-			<CssBaseline />
-			<Box sx={{ display: 'flex', mt: '30px' }}>
-				<Container sx={{ width: '40%', ml: '30px' }}>
-					<Sidebar logout={logout} />
-				</Container>
+        <Container
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+            paddingBottom: "10px",
+            marginTop: "20px",
+          }}
+        >
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{ display: "flex", gap: "10px", alignItems: "center" }}
+          >
+            <TextField
+              size="small"
+              placeholder="Add Contact..."
+              id="addContact"
+              name="addContact"
+              label="Add Contact"
+            />
+            <IconButton type="submit">
+              <SearchIcon />
+            </IconButton>
+          </Box>
+          <Typography variant="h6">
+            Select a few contacts to share your trip with:
+          </Typography>
 
-				<Container sx={{ display: 'flex', flexDirection: 'column', gap: '10px', paddingBottom: '10px', marginTop: '20px' }}>
-					<Box component="form" onSubmit={handleSubmit} sx={{ display: 'flex', gap: '10px', alignItems: 'center' }}>
-						<TextField size="small" placeholder="Add Contact..." id="addContact" name="addContact" label="Add Contact" />
-						<IconButton type="submit">
-							<SearchIcon />
-						</IconButton>
-					</Box>
-					<Typography variant="h6">Select a few contacts to share your trip with:</Typography>
+          <ContactList
+            contacts={contacts}
+            deleteContact={deleteContact}
+            checkedContacts={checkedContacts}
+            setCheckedContacts={setCheckedContacts}
+            setButtonDisabled={setButtonDisabled}
+          />
 
-					<ContactList contacts={contacts} deleteContact={deleteContact} checkedContacts={checkedContacts} setCheckedContacts={setCheckedContacts} setButtonDisabled={setButtonDisabled} />
-
-					<Button variant="contained" onClick={handleStartTrip} disabled={buttonDisabled}>
-						Start Your Trip!
-					</Button>
-				</Container>
-			</Box>
-		</>
-	);
+          <Button
+            variant="contained"
+            onClick={handleStartTrip}
+            disabled={buttonDisabled}
+          >
+            Start Your Trip!
+          </Button>
+        </Container>
+      </Box>
+    </>
+  );
 }
 
 export default Contacts;
 
 {
-	/* <Box sx={{ display: 'flex', mt: '30px' }}>
+  /* <Box sx={{ display: 'flex', mt: '30px' }}>
 <Container sx={{ width: '40%', ml: '30px' }}>
 	<Sidebar logout={logout} />
 </Container>
--- TS_src/client/src/components/MapContainer.tsx
+++ TS_src/client/src/components/MapContainer.tsx
@@ -1,35 +1,40 @@
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
-import React, { useState } from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import React, { useState } from "react";
 
 const containerStyle = {
-	height: '300px',
-	width: '700px',
+  height: "300px",
+  width: "700px",
 };
 
-function MapComponent({ userTrip }: any) {
-	const { isLoaded } = useJsApiLoader({
-		id: process.env.GOOGLEMAPSID,
-		googleMapsApiKey: process.env.GOOGLEMAPSAPIKEY,
-	});
+function MapComponent({ trip }: any) {
+  const { isLoaded } = useJsApiLoader({
+    id: process.env.GOOGLEMAPSID,
+    googleMapsApiKey: process.env.GOOGLEMAPSAPIKEY,
+  });
 
-	const center = {
-		lat: userTrip.start_lat,
-		lng: userTrip.start_lng,
-	};
+  const center = {
+    lat: trip.start_lat,
+    lng: trip.start_lng,
+  };
 
-	const [map, setMap] = useState(null);
+  const [map, setMap] = useState(null);
 
-	const onUnmount = React.useCallback(function callback(map: any) {
-		setMap(null);
-	}, []);
+  const onUnmount = React.useCallback(function callback(map: any) {
+    setMap(null);
+  }, []);
 
-	return isLoaded ? (
-		<GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12} onUnmount={onUnmount}>
-			<Marker position={center}></Marker>
-		</GoogleMap>
-	) : (
-		<></>
-	);
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={12}
+      onUnmount={onUnmount}
+    >
+      <Marker position={center}></Marker>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 }
 
 export default MapComponent;
