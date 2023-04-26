2fdde73698a2d7c9a6bb0d34c90281d43f03c9fb
Jenny Schmalz <jschmalzperez@gmail.com>
2023-04-25 19:47:28 -0700

    add __tests__ dir and configure package.json w/ vitest

---
+++ TS_src/__tests__/contactsController.test.js
@@ -0,0 +1,7 @@
+import { it, expect, describe } from 'vitest';
+
+import { contactsController } from '../server/controllers/contactsController';
+
+describe('contactsController.getContacts()', () => {
+
+});
--- TS_src/client/vite.config.ts
+++ TS_src/client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import EnvironmentPlugin from 'vite-plugin-environment';
 import react from '@vitejs/plugin-react';
+/// <reference types="vitest" />
 
 // https://vitejs.dev/config/
 export default defineConfig({
