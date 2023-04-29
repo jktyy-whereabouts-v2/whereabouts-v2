b2b056264da0aa1055521d7116cf24de37b504b3
Yourui <yourui.ruan@gmail.com>
2023-04-28 20:25:00 -0400

    Took out old test routes for sending texts

--- TS_src/server/controllers/twilioController.ts
+++ TS_src/server/controllers/twilioController.ts
@@ -2,25 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 import twilio from 'twilio';
 
 const twilioController = {
-    testNotif: async (req: Request, res: Response, next: NextFunction) => {
-        // instantiate twilio with twilio SID and token from registering with twilio
-        const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
-        try {
-            const message = await client.messages.create({
-                body: 'hello from twilio',
-                from: process.env.TWILIO_NUMBER,
-                to: '+15615813162'
-            });
-            res.locals.message = message;
-            console.log(message);
-            return next()
-        } catch(error) {
-            console.log(error);
-            return next({log: 'error at twilioController.testNotif', message: 'problem sending twilio text'})
-        }
-    },
-
     startTripNotif: async (req: Request, res: Response, next: NextFunction) => {
+        // instantiate twilio with twilio SID and token from registering with twilio
         const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
         try {
             const { name } = req.query;
@@ -36,13 +19,13 @@ const twilioController = {
                     // send twilio message
                     console.log(`sending a message to ${contacts[i]}`)
                     const message = await client.messages.create({
-                        body: `${name} trip started! Reply STOP to opt-out`,
+                        body: `${name} started a trip! Reply STOP to opt-out`,
                         from: process.env.TWILIO_NUMBER,
                         to: contacts[i]
                     })
                 }
             }
-            console.log('finished sending messages')
+            console.log('finished sending start trip messages')
             return next()
         } catch (error) {
             console.log(error);
@@ -51,6 +34,7 @@ const twilioController = {
     },
 
     endTripNotif: async (req: Request, res: Response, next: NextFunction) => {
+        // instantiate twilio with twilio SID and token from registering with twilio
         const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
         try {
             const { name } = req.query;
@@ -60,24 +44,18 @@ const twilioController = {
             })
 
             // iterate over array of contacts and create message for each contact
-            // for (let i = 0; i < contacts.length; i++) {
-            //     //validation to ensure phone number is correct length for real phone numbers
-            //     if (contacts[i].length == 12) {
-            //         // send twilio message
-            //         console.log(`sending a message to ${contacts[i]}`)
-            //         const message = await client.messages.create({
-            //             body: `${name} arrived safely at their destination. Reply STOP to opt-out`,
-            //             from: process.env.TWILIO_NUMBER,
-            //             to: contacts[i]
-            //         })
-            //     }
-            // }
-
-            const message = await client.messages.create({
-                body: `${name} arrived safely at their destination. Reply STOP to opt-out`,
-                from: process.env.TWILIO_NUMBER,
-                to: contacts[0]
-            })
+            for (let i = 0; i < contacts.length; i++) {
+                //validation to ensure phone number is correct length for real phone numbers
+                if (contacts[i].length == 12) {
+                    // send twilio message
+                    console.log(`sending a message to ${contacts[i]}`)
+                    const message = await client.messages.create({
+                        body: `${name} arrived safely at their destination. Reply STOP to opt-out`,
+                        from: process.env.TWILIO_NUMBER,
+                        to: contacts[i]
+                    })
+                }
+            }
             console.log('finished sending end trip messages')
             return next()
         } catch (error) {
--- TS_src/server/routers/notifRouter.ts
+++ TS_src/server/routers/notifRouter.ts
@@ -3,10 +3,6 @@ import twilioController from '../controllers/twilioController'
 const contactsController = require('../controllers/contactsController');
 const router = Router();
 
-router.get('/', twilioController.testNotif, (req: Request, res: Response) => {
-    return res.status(200).json(res.locals.message)
-})
-
 router.get('/startNotif/', contactsController.getContacts, twilioController.startTripNotif, (req: Request, res: Response, next: NextFunction) => {
     return res.status(200).json('sent start trip text')
 })
